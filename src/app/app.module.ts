b31d5e2d89cd0df01b40d51ede8ed22020ec296d
Agustin Simon <agustinsimon@MBP-de-Agustin.fibertel.com.ar>
2022-10-15 22:41:55 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { HomeComponent } from '../components/home/home.component';
+import { AcercaComponent } from '../components/acerca/acerca.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AcercaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'acerca', component:AcercaComponent},
+      {path:'**', redirectTo:'/', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
